0f9a24a0c4c8c6b40e39c3ad39ca1af80bb54e96
root <root@ip-172-31-43-201.us-west-2.compute.internal>
2019-06-09 13:35:40 +0000

    Change README file

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'codeez',         # How you named your package folder (MyLib)
   packages = ['codeez'],   # Chose the same as "name"
   version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'codeez python SDK',   # Give a short description about your library
+  description = 'codeez python SDK',
+  long_description=long_description,
+  long_description_content_type='text/markdown' # Give a short description about your library
   author = 'Antoine Larmanjat',                   # Type in your name
   author_email = 'antoine_larmanjat@hotmail.com',      # Type in your E-Mail
   url = 'https://www.github.com/antoinelarmanjat/codeez',   # Provide either the link to your github or to your website
-  download_url = 'https://www.github.com/antoinelarmanjat/codeez/archive/v_05.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/antoinelarmanjat/codeez/archive/0.5.tar.gz',    # I explain this later on
   keywords = ['CODEEZ', 'SDK', 'PYTHON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
