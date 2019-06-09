a959de9fa63b3d4c8fee71600aa2e1bd3835e78f
root <root@ip-172-31-43-201.us-west-2.compute.internal>
2019-06-09 15:16:10 +0000

    Fix READM

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from distutils.core import setup
 
 # read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
   name = 'codeez',         # How you named your package folder (MyLib)
   packages = ['codeez'],   # Chose the same as "name"
-  version = '0.6',      # Start with a small number and increase it with every change you make
+  version = '0.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'codeez python SDK',
   long_description=long_description,
@@ -17,7 +13,7 @@ setup(
   author = 'Antoine Larmanjat',                   # Type in your name
   author_email = 'antoine_larmanjat@hotmail.com',      # Type in your E-Mail
   url = 'https://www.github.com/antoinelarmanjat/codeez',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/antoinelarmanjat/codeez/archive/0.6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/antoinelarmanjat/codeez/archive/0.7.tar.gz',    # I explain this later on
   keywords = ['CODEEZ', 'SDK', 'PYTHON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
