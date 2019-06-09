8bef3acbffe7f10e55dfe4dbe5a7a0e293bfe34a
root <root@ip-172-31-43-201.us-west-2.compute.internal>
2019-06-09 13:43:55 +0000

    Fix setup to 0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
   author = 'Antoine Larmanjat',                   # Type in your name
   author_email = 'antoine_larmanjat@hotmail.com',      # Type in your E-Mail
   url = 'https://www.github.com/antoinelarmanjat/codeez',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/antoinelarmanjat/codeez/archive/0.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/antoinelarmanjat/codeez/archive/0.6.tar.gz',    # I explain this later on
   keywords = ['CODEEZ', 'SDK', 'PYTHON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
